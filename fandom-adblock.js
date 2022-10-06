1e3a5640704a377ffef34406bbe7f2cda9975ed5
Buggem <66670928+Buggem@users.noreply.github.com>
2022-10-06 11:16:47 +1100

    Update fandom-adblock.js

--- fandom-adblock.js
+++ fandom-adblock.js
@@ -2,3 +2,4 @@ var naughtyelems = document.getElementsByClassName("gpt-ad");
 for(let i = 0; i < naughtyelems.length; i++) {
     naughtyelems[i].remove();
 } 
+document.getElementById("WikiaBar").remove();
